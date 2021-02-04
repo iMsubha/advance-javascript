1bb604c9b5d4e32d69ab41f90420a643ca31fb3b
iMsubha <subha.fairuz@gmail.com>
2021-02-04 13:25:13 +0600

    first commit

---
+++ true-false.js
@@ -0,0 +1,16 @@
+const age = -4;
+const fname = "Subha";
+// if(age){
+//     console.log("condition true");
+// }else{
+//     console.log("condition false");
+// }
+//console.log(typeof age);
+
+ 
+console.log((age)? "true": "false");
+console.log((fname.length > 0)? "true": "false");
+console.log((NaN)? "NaN: true": "NaN: false");
+console.log((null)? "null: true": "null: false");
+console.log((undefined)? "undefined: true": "undefined: false");
+
